bb60ee8936dc5bc504b1ec58a5c3f17c865fdd60
Austin <AnonymousAJX@Gmail.com>
2019-03-04 22:04:32 -0500

    Added dialogue and notebook stuff
    Now the scene has dialogue between the two characters, the ability to check the BookFace page via projector, and the ability to check the notebook for notes. These changes are all in the Demo2 scene.

--- Spectacle-Squad-master/Entity_1/Assets/Scripts/PlayerController.cs
+++ Spectacle-Squad-master/Entity_1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 
     Animator anim;
     Rigidbody rgb;
+    PlayerInteraction pli;
     public float speed = 5;
     public float rotateSpeed = 90;
     public Vector3 moveDirection = Vector3.zero;
@@ -14,42 +15,44 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
         anim = GetComponent<Animator>();
         rgb = GetComponent<Rigidbody>();
+        pli = GetComponent<PlayerInteraction>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+    // Update is called once per frame
+    void Update() {
+        if (!pli.InConversation) {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
 
-        if (v < 0)
-        {
-            v = 0;
-        }
+            if (v < 0)
+            {
+                v = 0;
+            }
 
-        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)))
-        {
-            transform.Rotate(Vector3.up, Mathf.Clamp(180f * Time.deltaTime, 0f, 360f));
-        }
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)))
-        {
-            transform.Rotate(Vector3.up, -Mathf.Clamp(180f * Time.deltaTime, 0f, 360f));
-        }
+            if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)))
+            {
+                transform.Rotate(Vector3.up, Mathf.Clamp(180f * Time.deltaTime, 0f, 360f));
+            }
+            if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)))
+            {
+                transform.Rotate(Vector3.up, -Mathf.Clamp(180f * Time.deltaTime, 0f, 360f));
+            }
 
-        if (v != 0)
-        {
-            anim.SetBool("Walking", true);
-        }
-        else
-        {
-            anim.SetBool("Walking", false);
-        }
-       
-        moveDirection = new Vector3(0, 0, v);
-        moveDirection = transform.TransformDirection(moveDirection);
-        moveDirection *= speed;
+            if (v != 0)
+            {
+                anim.SetBool("Walking", true);
+            }
+            else
+            {
+                anim.SetBool("Walking", false);
+            }
 
-        rgb.velocity = moveDirection;   
+            moveDirection = new Vector3(0, 0, v);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            rgb.velocity = moveDirection;
+        }
     }
 }
 
--- Spectacle-Squad-master/Entity_1/Assets/Scripts/PlayerInteraction.cs
+++ Spectacle-Squad-master/Entity_1/Assets/Scripts/PlayerInteraction.cs
@@ -9,34 +9,43 @@ public class PlayerInteraction : MonoBehaviour {
     public float distanceToGirl = 5f;
     public Transform computer;
     public float distanceToComputer = 5f;
-
+    public GameObject BookFacePost;
+    public Image Notebook;
+    public Image SpeechBubble;
+    public Text Dialogue;
     string[] dialogue;
-    int dialogueIndex;
-    public Text notepad;
+    int dialogueIndex = 0;
+    //public Text notepad;
     public Text notepad_girl;
     public Text notepad_computer;
     bool talkedToGirl;
     bool seenComputer;
+    [HideInInspector]
+    public bool InConversation;
+    bool ComputerON;
     public Text computerPost;
+    public GameObject newnoteadded;
 
     // The notepad and the social network post can be done via UI text
 
 	void Start () {
         dialogueIndex = 0;
-        dialogue = new string[10];
+        dialogue = new string[12];
         dialogue[0] = "Player: Hey, how’s it going?";
         dialogue[1] = "Girl: I can’t believe he’s doing this again! ";
         dialogue[2] = "Player: Who? ";
         dialogue[3] = "Girl: Mr. Thompson! He gave me and the other girls penalties for being late again.";
         dialogue[4] = "Player: That doesn’t seem unreasonable. ";
         dialogue[5] = "Girl: You don’t get it! The boys are late all the time because of morning sports " +
-            "practices, but he never says anything to them. But if the girls’ practice runs for too long he " +
+            "practices, but he never says anything to them.";
+        dialogue[6] = "Girl: But if the girls’ practice runs for too long he " +
             "marks us all as late! It’s not fair! ";
-        dialogue[6] = "Player: Has anyone tried doing anything?";
-        dialogue[7] = " We told our coaches but there’s not much they can do.";
-        dialogue[8] = "Player: Hey, I’m actually writing an article about implicit bias in the school, " +
-            "do you mind if I use this story? "
-        dialogue[9] = "Girl: If it helps me and the other girls stop getting tardy notices, sure.";
+        dialogue[7] = "Player: Has anyone tried doing anything?";
+        dialogue[8] = "Girl: We told our coaches but there’s not much they can do.";
+        dialogue[9] = "Player: Hey, I’m actually writing an article about implicit bias in the school, " +
+            "do you mind if I use this story? ";
+        dialogue[10] = "Girl: If it helps me and the other girls stop getting tardy notices, sure.";
+        dialogue[11] = "";
 
     }
 	
@@ -44,24 +53,83 @@ public class PlayerInteraction : MonoBehaviour {
 	void Update () {
 
         // Interaction with NPC and Object
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Vector3.Distance(transform.position, girl.position) <= distanceToGirl)
+            if (Vector3.Distance(transform.position, girl.position) <= distanceToGirl && dialogueIndex < 12)
             {
-
+                DialogueFunction();
             }
 
-            if (Vector3.Distance(transform.position, computer.position) <= distanceToComputer)
+            if (Vector3.Distance(transform.position, computer.position) <= distanceToComputer && !ComputerON)
             {
-
+                BookFacePost.SetActive(true);
+                ComputerON = true;
             }
         }
 		
         // Notepad
-        if (Input.GetKey(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Tab) && !InConversation)
         {
-
+            if (Notebook.enabled)
+            {
+                Notebook.enabled = false;
+                notepad_girl.enabled = false;
+                notepad_computer.enabled = false;
+            }
+            else
+            {
+                Notebook.enabled = true;
+                if (talkedToGirl) notepad_girl.enabled = true;
+                if (seenComputer) notepad_computer.enabled = true;
+            }
+            
         }
 
+        if (ComputerON && Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!seenComputer)
+            {
+                StartCoroutine(newnoteblip(4));
+                seenComputer = true;
+            }
+            ComputerON = false;
+            BookFacePost.SetActive(false);
+        }
+
+
 	}
+
+
+    void DialogueFunction()
+    {
+        if (dialogueIndex <= 11)
+        {
+            InConversation = true;
+            SpeechBubble.enabled = true;
+            Notebook.enabled = false;
+            notepad_girl.enabled = false;
+            notepad_computer.enabled = false;
+            Dialogue.text = dialogue[dialogueIndex];
+            dialogueIndex = dialogueIndex + 1;
+
+
+        }
+        if (dialogueIndex == 12)
+        {
+            InConversation = false;
+            talkedToGirl = true;
+            SpeechBubble.enabled = false;
+            Dialogue.text = "";
+            StartCoroutine(newnoteblip(4));
+        }
+
+    }
+
+    IEnumerator newnoteblip(float waittime)
+    {
+        newnoteadded.transform.Translate(0, 125, 0);
+        yield return new WaitForSeconds(waittime);
+        newnoteadded.transform.Translate(0, -125, 0);
+    }
+
 }
